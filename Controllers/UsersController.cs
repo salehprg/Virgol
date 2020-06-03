8b61f8f957657e8d8f765c00c6fb0803f4e95bf2
saleh_prg <salehebrahimian@gmail.com>
2020-06-03 20:19:23 +0430

    Admin : ConfirmUser / SomeFilename Changed

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Novell.Directory.Ldap;
+
+namespace lms_with_moodle.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           LdapConnection connection = new LdapConnection();
+           connection.Connect("localhost" , 10389);
+           
+           return Ok();
+        }
+    }
+}
