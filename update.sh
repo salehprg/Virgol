5df7a8664ebdef5bbf85e55fd8f59e7d59b53e5d
Hamid <Goldenstarc@gmail.com>
2020-06-18 17:31:28 +0430

    auto_update

---
+++ update.sh
@@ -0,0 +1,11 @@
+cd lms-with-moodle 
+sudo git pull
+docker build -t goldenstarc/lms:0.3 .
+docker stop lms_0.3
+docker rm lms_0.3
+docker run \
+    -p 5001:80 \
+    --restart=always \
+    --name=lms_0.3 \
+    -d goldenstarc/lms:0.3
+# docker push goldenstarc/lms:0.3
