ecb1da1c56daa296c0c56c20acf613f59851d8dc
Saleh Ebrahimian <salehebrahimian@gmail.com>
2020-06-29 15:25:04 +0000

    Update ApiConfig.js

--- src/ClientApp/src/_Services/ApiConfig.js
+++ src/ClientApp/src/_Services/ApiConfig.js
@@ -1,6 +1,6 @@
 export const ApiConfig = {
 //BaseUrl : "https://localhost:5001/api/",
-BaseUrl : "http://lms.legace.ir/api/",
+BaseUrl : "https://lms.legace.ir/api/",
 AdminUrl : "Admin/",
 UserUrl : "Users/"
 };
