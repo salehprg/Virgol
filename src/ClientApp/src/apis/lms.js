94ee99d2f1500701077e99e828a43cf14a9b70cf
Hamid <Goldenstarc@gmail.com>
2020-07-01 06:47:40 +0430

    localhost:5001

--- src/ClientApp/src/apis/lms.js
+++ src/ClientApp/src/apis/lms.js
@@ -2,5 +2,6 @@ import Axios from "axios";
 import { config } from "../assets/constants";
 
 export default Axios.create({
-    baseURL: 'https://localhost:5001/'
+    // baseURL: 'https://localhost:5001/'
+    baseURL: 'https://lms.legace.ir/'
 });
