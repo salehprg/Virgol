783364328129ec5fd5568569e7ea0cc7a42b6aef
Javad Moghaddam <sed.javadmoghaddam@gmail.com>
2020-07-05 15:26:14 +0000

    Update lms.js

--- src/Presentation/Virgol.School/ClientApp/src/apis/lms.js
+++ src/Presentation/Virgol.School/ClientApp/src/apis/lms.js
@@ -2,6 +2,5 @@ import Axios from "axios";
 import { config } from "../assets/constants";
 
 export default Axios.create({
-    //baseURL: 'https://lms.legace.ir'
-    baseURL: 'https://localhost:5001'
+    baseURL: config.url.APU_URL
 });
