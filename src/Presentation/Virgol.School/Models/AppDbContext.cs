56ba30587de58cf29a419b8d388009345dde2481
saleh_prg <salehebrahimian@gmail.com>
2020-07-19 22:19:21 +0430

    Db Updated

--- src/Presentation/Virgol.School/Models/AppDbContext.cs
+++ src/Presentation/Virgol.School/Models/AppDbContext.cs
@@ -21,6 +21,8 @@ namespace Models
         public DbSet<Meeting> Meetings {get; set;}
         public DbSet<ParticipantInfo> ParticipantInfos {get; set;}
         public DbSet<UserDetail> UserDetails {get; set;}
+        public DbSet<SchoolModel> Schools {get; set;}
+        public DbSet<Manager> Managers {get; set;}
 
 
         // protected override void OnModelCreating(ModelBuilder builder)
