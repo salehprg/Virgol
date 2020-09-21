905ac87871ab5f27ba54f5625bbbebb0735935d5
saleh_prg <salehebrahimian@gmail.com>
2020-09-21 12:19:43 +0330

    Meeting check job fixed

--- src/Presentation/Virgol.School/Schedule/Check Attendee in class/CheckAttendeeJob.cs
+++ src/Presentation/Virgol.School/Schedule/Check Attendee in class/CheckAttendeeJob.cs
@@ -49,33 +49,33 @@ namespace Schedule
                         
                         foreach(var newMeeting in newMeetingList)
                         {
-                            MeetingView oldMeeting = oldMeetingList.Where(x => x.BBB_MeetingId == newMeeting.meetingID && !x.Finished).FirstOrDefault();
-                            if(oldMeeting != null)
+                            MeetingView oldMeetingVW = oldMeetingList.Where(x => x.BBB_MeetingId == newMeeting.meetingID && !x.Finished).FirstOrDefault();
+                            if(oldMeetingVW != null)
                             {
-                                Meeting oldMeetingInfo = dbContext.Meetings.Where(x => x.Id == oldMeeting.Id).FirstOrDefault();
+                                Meeting oldMeetingInfo = dbContext.Meetings.Where(x => x.Id == oldMeetingVW.Id).FirstOrDefault();
                                 if(!oldMeetingInfo.Private) // it means current Meeting exist and active in our database 
                                 {
                                     foreach(var attendee in newMeeting.attendees.attendee.Where(x => x.role != "MODERATOR")) // Participant present in Online Course
                                     {
-                                        ParticipantInfo participantInfo = dbContext.ParticipantInfos.Where(x => x.MeetingId == oldMeeting.Id && x.UserId == attendee.userID).FirstOrDefault();
+                                        ParticipantInfo participantInfo = dbContext.ParticipantInfos.Where(x => x.MeetingId == oldMeetingVW.Id && x.UserId == attendee.userID).FirstOrDefault();
                                         if(participantInfo != null)
                                         {
                                             participantInfo.PresentCount++;
-                                            participantInfo.IsPresent = (participantInfo.PresentCount / (oldMeeting.CheckCount + 1) * 100 ) > 70 ? true : false;
+                                            participantInfo.IsPresent = (participantInfo.PresentCount / (oldMeetingVW.CheckCount + 1) * 100 ) > 70 ? true : false;
                                             dbContext.ParticipantInfos.Update(participantInfo);
                                         }
                                         else
                                         {
                                             ParticipantInfo newAttendee = new ParticipantInfo();
-                                            newAttendee.MeetingId = oldMeeting.Id;
+                                            newAttendee.MeetingId = oldMeetingVW.Id;
                                             newAttendee.UserId = attendee.userID;
                                             newAttendee.PresentCount = 1;
 
                                             dbContext.ParticipantInfos.Add(newAttendee);
                                         }
                                     }
-                                    oldMeeting.CheckCount++;
-                                    dbContext.Update(oldMeeting);
+                                    oldMeetingInfo.CheckCount++;
+                                    dbContext.Update(oldMeetingInfo);
                                 }
                             }
                             //use this for sync with moodle
@@ -92,7 +92,9 @@ namespace Schedule
                             // }
                         }
 
-                        foreach(var oldMeeting in oldMeetingList.Where(x => !x.Finished))
+                        oldMeetingList = oldMeetingList.Where(x => !x.Finished).ToList();
+
+                        foreach(var oldMeeting in oldMeetingList)
                         {
                             var closedMeeting = newMeetingList.Where(x => x.meetingID == oldMeeting.BBB_MeetingId).FirstOrDefault();
                             if(closedMeeting == null) // it means a meeting has closed but its state in our database is Active
