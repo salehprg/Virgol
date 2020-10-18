2b72dca0e2d83c5d538a7b6e27d93cd35214ddf5
Saleh Ebrahimian <salehebrahimian@gmail.com>
2020-10-18 13:49:01 +0000

    Update i18n.js

--- src/Presentation/Virgol.School/ClientApp/src/i18n.js
+++ src/Presentation/Virgol.School/ClientApp/src/i18n.js
@@ -12,7 +12,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "fa",
+    fallbackLng: "ar",
     debug: false,
 
     interpolation: {
