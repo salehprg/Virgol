1842ede4c47314910c4877de675ffe1d5d5ef59f
saleh_prg <salehebrahimian@gmail.com>
2020-11-04 20:23:35 +0330

    Redirect bug fied

---
+++ src/Presentation/Virgol.School/Services/CustomAuthHandler/AuthPolicyHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class AuthPolicyHandler : IAuthorizationPolicyProvider
+{
+    public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+
+    public AuthPolicyHandler(IOptions<AuthorizationOptions> options)
+    {
+        FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+    }
+
+    public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => FallbackPolicyProvider.GetDefaultPolicyAsync();
+
+    public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+    {
+
+        var policy = new AuthorizationPolicyBuilder();
+        policy.AddRequirements(new TokenRequirement(policyName));
+        return Task.FromResult(policy.Build());
+    }
+
+    public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+    {
+        return FallbackPolicyProvider.GetFallbackPolicyAsync();
+    }
+}
