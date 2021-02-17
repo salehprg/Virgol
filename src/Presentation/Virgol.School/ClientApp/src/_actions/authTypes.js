63aa1b6957132e91edc2771facb5d2818a9ee6f0
mzhb80 <mzhb13801398@gmail.com>
2021-02-17 21:05:22 +0330

    captcha prepare

--- src/Presentation/Virgol.School/ClientApp/src/_actions/authTypes.js
+++ src/Presentation/Virgol.School/ClientApp/src/_actions/authTypes.js
@@ -2,3 +2,4 @@ export const LOGIN = 'LOGIN';
 export const USER_STATUS = 'USER_STATUS';
 export const LOGOUT = 'LOGOUT';
 export const SEND_CODE = 'SEND_CODE';
+export const INCREASE_EFFORT = 'INCREASE_EFFORT'
