a77331e68416b2ffef54ca43e68fb79e6262eaba
mzhb80 <mzhb13801398@gmail.com>
2021-02-18 15:21:52 +0330

    some change in side bar

--- src/Presentation/Virgol.School/ClientApp/src/components/dashboards/sidebar/SidebarCard.js
+++ src/Presentation/Virgol.School/ClientApp/src/components/dashboards/sidebar/SidebarCard.js
@@ -4,7 +4,7 @@ const SidebarCard = ({ active, code, title, icon, changeActive }) => {
 
     return (
         <div onClick={() => changeActive(code)} className={`tw-w-full tw-my-3 tw-cursor-pointer tw-mx-auto tw-px-4 tw-py-3 tw-flex tw-flex-row tw-justify-end tw-items-center tw-rounded-xl tw-transform tw-duration-200 ${active === code ? 'tw-bg-purplish' : 'tw-bg-transparent'}`}>
-            <span className="tw-text-xl tw-text-right tw-text-white tw-mr-2">{title}</span>
+            <span className="my-text tw-text-right tw-text-white tw-mr-2">{title}</span>
             {icon('tw-w-8 tw-text-white')}
         </div>
     );
