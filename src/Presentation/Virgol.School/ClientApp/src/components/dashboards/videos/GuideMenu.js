47efb779918dfce95ec4bc520d51f8554d6c0657
Javad Moghaddam <sed.javadmoghaddam@gmail.com>
2021-03-10 16:02:59 +0330

    introduction videos changes

--- src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/GuideMenu.js
+++ src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/GuideMenu.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import SelectableGuide from './utils/SelectableGuide';
-import studentPic from '../../../assets/pics/studentVector.png'
-import teacherPic from '../../../assets/pics/teacherVector.png'
-import managerPic from '../../../assets/pics/managerVector.png'
+import studentPic from '../../../assets/pics/studenting.svg'
+import teacherPic from '../../../assets/pics/teaching.svg'
+import managerPic from '../../../assets/pics/managing.svg'
 import { arrow_left } from '../../../assets/icons';
 import history from '../../../history';
 
 const GuideMenu = () => {
     return(
-        <div className="tw-bg-dark-blue">  
+        <div className="tw-bg-dark-blue tw-min-h-screen">  
             <div className="tw-p-4 tw-cursor-pointer" onClick={() => history.push('/')}>{arrow_left('tw-border-2 tw-cursor-pointer tw-border-purplish tw-p-2 tw-rounded-lg tw-text-purplish tw-w-12')}</div>
             <div className="tw-text-center">
                 <div className="tw-text-white tw-py-12 tw-text-xl">یکی از گزینه های زیر را انتخاب کنید</div>
-                <div className="tw-grid tw-grid-rows-3 tw-gap-4 lg:tw-mx-48 md:tw-mx-24 tw-mx-12 md:tw-grid-cols-3">
-                    <SelectableGuide src={studentPic} title="راهنمای ویژه دانش آموزان" type="student"/>
-                    <SelectableGuide src={teacherPic} title="راهنمای ویژه معلمان" type="teacher"/>
-                    <SelectableGuide src={managerPic} title="راهنمای ویژه مدیران" type="principal"/>
+                <div className="tw-grid tw-grid-rows-1 tw-gap-4 lg:tw-mx-48 md:tw-mx-24 tw-mx-12 md:tw-grid-cols-3">
+                    <SelectableGuide src={studentPic} extra="tw-w-5/6" title="راهنمای ویژه دانش آموزان" type="student"/>
+                    <SelectableGuide src={teacherPic} extra="tw-w-11/12" title="راهنمای ویژه معلمان" type="teacher"/>
+                    <SelectableGuide src={managerPic} extra="tw-w-2/3" title="راهنمای ویژه مدیران" type="principal"/>
                 </div>
             </div>
         </div>
--- src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/utils/SelectableGuide.js
+++ src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/utils/SelectableGuide.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import history from '../../../../history';
 
-const SelectableGuide = ({title , src , type}) => {
+const SelectableGuide = ({title , src , type, extra}) => {
     return(
-        <div className="tw-text-center tw-border-4 tw-border-purplish tw-rounded-lg tw-cursor-pointer hover:tw-shadow-xl hover:tw-border-greenish" onClick={() => history.push(`/video/${type}-guide-pr`)}>
-            <img src={src} alt="picture"/>
+        <div className="tw-flex tw-flex-col tw-justify-between tw-items-center tw-p-8 tw-border-4 tw-border-purplish tw-rounded-lg tw-cursor-pointer hover:tw-shadow-xl hover:tw-border-greenish" onClick={() => history.push(`/video/${type}-guide-pr`)}>
+            <img className={extra} src={src} alt="picture"/>
             <div className="tw-text-white tw-py-4 tw-text-xl">{title}</div>
         </div>
     )
