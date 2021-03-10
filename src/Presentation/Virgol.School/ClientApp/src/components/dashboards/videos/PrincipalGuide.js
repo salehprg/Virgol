d3c07531ae5d7e90ad113635b81a3c790e137f21
mzhb80 <mzhb13801398@gmail.com>
2021-03-10 11:01:19 +0330

    guide pages improved for safari

--- src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/PrincipalGuide.js
+++ src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/PrincipalGuide.js
@@ -7,12 +7,12 @@ class PrincipalGuide extends React.Component{
     render(){    
         const url = 'https://goldenstarc.arvanvod.com/0WobRajx6K/pZq3EW9vdj/h_,144_200,240_400,360_502,480_502,720_502,1080_502,k.mp4.list/master.m3u8'
         return(
-            <div className="tw-w-full tw-h-screen tw-bg-vst tw-grid tw-grid-rows-4 md:tw-grid-cols-2">
-                <div>
+            <div className="tw-w-full tw-h-screen tw-bg-vst row">
+                <div className="col-lg-6 col-md-12 col-xl-6 col-sm-12">
                     <div className="tw-m-4" onClick={()=> history.push('/video/guide-pr')}>{arrow_left('tw-w-12 tw-border-2 tw-text-purplish tw-border-purplish tw-rounded-lg tw-cursor-pointer tw-mx-2 tw-p-2')}</div>
                     <div className="tw-text-white tw-mx-auto tw-w-1/2 tw-text-center tw-py-4 tw-border-4 tw-text-sm lg:tw-text-lg tw-border-purplish tw-rounded-lg">راهنمای سامانه ویرگول مخصوص مدیران</div>  
                 </div>
-                <div className="tw-rows-span-3">
+                <div className="col-lg-6 col-md-12 col-xl-6 col-sm-12">
                     <ReactPlayer className="tw-mx-auto lg:tw-mt-24 tw-mb-4" url={url} width='90%' controls={true}/>
                     <form method="get" action="https://goldenstarc.arvanvod.com/0WobRajx6K/pZq3EW9vdj/origin_FjJsk5SRrvgYje8m395IVK6hkEQOJWiI3iSBTTrM.mp4" className="tw-text-center">
                         <input className="tw-px-6 tw-cursor-pointer tw-bg-transparent tw-py-1 tw-border-2 tw-border-sky-blue tw-text-sky-blue tw-rounded-lg tw-mx-auto" value={this.props.t('download')} type="submit"/>
--- src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/TeacherGuide.js
+++ src/Presentation/Virgol.School/ClientApp/src/components/dashboards/videos/TeacherGuide.js
@@ -7,12 +7,12 @@ class TeacherGuide extends React.Component{
     render(){    
         const url = 'https://goldenstarc.arvanvod.com/0WobRajx6K/ka5q25YdOv/h_,144_200,240_400,360_793,480_793,720_793,1080_793,k.mp4.list/master.m3u8'
         return(
-            <div className="tw-w-full tw-h-screen tw-bg-vst tw-grid tw-grid-rows-4 md:tw-grid-cols-2">
-                <div>
-                <div className="tw-m-4" onClick={()=> history.push('/video/guide-pr')}>{arrow_left('tw-w-12 tw-border-2 tw-text-purplish tw-border-purplish tw-rounded-lg tw-cursor-pointer tw-mx-2 tw-p-2')}</div>
+            <div className="tw-w-full tw-h-screen tw-bg-vst row">
+                <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <div className="tw-m-4" onClick={()=> history.push('/video/guide-pr')}>{arrow_left('tw-w-12 tw-border-2 tw-text-purplish tw-border-purplish tw-rounded-lg tw-cursor-pointer tw-mx-2 tw-p-2')}</div>
                     <div className="tw-text-white tw-mx-auto tw-w-1/2 tw-text-center tw-py-4 tw-border-4 tw-text-sm lg:tw-text-lg tw-border-purplish tw-rounded-lg">راهنمای سامانه ویرگول مخصوص معلمان</div>  
                 </div>
-                <div className="tw-rows-span-3">
+                <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
                     <ReactPlayer className="tw-mx-auto lg:tw-mt-24 tw-mb-4" url={url} width='90%' controls={true}/>
                     <form method="get" action="https://goldenstarc.arvanvod.com/0WobRajx6K/ka5q25YdOv/origin_j2qtq32HP3qzc4bCQrlwvDJfJUXYjJ9KQE2k0BtK.mp4" className="tw-text-center">
                         <input className="tw-px-6 tw-cursor-pointer tw-bg-transparent tw-py-1 tw-border-2 tw-border-sky-blue tw-text-sky-blue tw-rounded-lg tw-mx-auto" value={this.props.t('download')} type="submit"/>
